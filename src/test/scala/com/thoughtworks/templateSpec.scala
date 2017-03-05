832a91662e837ac64e21008d8844cd78e20dab9a
Yang Bo <atryyang@thoughtworks.com>
2017-03-05 12:28:39 +0800

    Add messages for test cases

--- src/test/scala/com/thoughtworks/templateSpec.scala
+++ src/test/scala/com/thoughtworks/templateSpec.scala
@@ -8,34 +8,52 @@ import org.scalatest.{FreeSpec, Matchers}
 final class templateSpec extends FreeSpec with Matchers {
 
   @template
-  def plus(x: Any, y: Int) = x + y
+  def plus(x: Any, y: Int with Singleton) = x + y
 
   plus(2, 3) should be(5)
   plus("2", 3) should be("23")
 
-  "plus(2, ???)" should compile
-  """plus(2, "not a Int")""" shouldNot typeCheck
+  "`Int with Singleton` parameter" - {
+    "should accept `???`" in {
+      "plus(math.random, ???)" should compile
+    }
+    "should not accept non-stable expression `math.random.toInt`" in {
+      "plus(math.random, math.random.toInt)" shouldNot compile
+    }
+    "should not accept String" in {
+      """plus(2, "not a Int")""" shouldNot typeCheck
+    }
+  }
 
-  @template
-  def plus2(x: Any, y: Any): String = x + y
+  "A template function returns a String" - {
+    @template
+    def concat(x: Any, y: Any): String = x + y
 
-  plus2(2, "m") should be("2m")
-  plus2("$", 3) should be("$3")
+    "should compile for String concatenation" in {
+      concat(2, "m") should be("2m")
+      concat("$", 3) should be("$3")
+    }
 
-  // Disable this line because ScalaTest does not support it.
-  // "plus2(2, 3)" shouldNot compile
+    "should not compile for Double" in {
+      // Disable this line because ScalaTest does not support it.
+       "plus2(1.0, 3.5)" should compile
+    }
+  }
 
-  "HList should index by Int literal" in {
-    import templateSpec._
-    val hlist = "foo" :: 1 :: false :: HNil
+  "HList" - {
+    "should index by Int literal" in {
+      import templateSpec._
+      val hlist = "foo" :: 1 :: false :: HNil
 
-    hlist(0) should be("foo")
-    hlist(1) should be(1)
-    hlist(2) should be(false)
-    "hlist(3)" shouldNot typeCheck
+      hlist(0) should be("foo")
+      hlist(1) should be(1)
+      hlist(2) should be(false)
+    }
+    "should not accept out of bound index" in {
+      "hlist(3)" shouldNot typeCheck
+    }
   }
 }
-
 object templateSpec {
 
   sealed trait HList {
